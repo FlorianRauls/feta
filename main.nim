3f0936305da86228c1a7ed8f6875126f26caa337
Florian Rauls <frauls@web.de>
2021-05-07 15:25:23 +0200

    Initiated Main/HelloWorld

---
+++ main.nim
@@ -0,0 +1,8 @@
+when system.hostOS == "windows":
+  echo "running on Windows!"
+elif system.hostOS == "linux":
+  echo "running on Linux!"
+elif system.hostOS == "macosx":
+  echo "running on Mac OS X!"
+else:
+  echo "unknown operating system"
