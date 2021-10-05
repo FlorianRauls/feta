6f36d82f3e8ab261f9533bafa945fae44655f4d3
Florian Rauls <34367590+FlorianRauls@users.noreply.github.com>
2021-10-05 20:05:25 +0200

    Delete userData.nim

--- feta/files/userData.nim
+++
@@ -1,9 +0,0 @@
-const userMail * = "carpi12350@googlemail.com" # The E-Mail you want to send your messages from
-
-const userMailWord * = "kwirfbvalvikherm" # Password of your E-Mail. Do NEVER keep this as plain text!!!
-
-const targetMailTest * = "f.rauls@stud.uni-heidelberg.de" # Debug Option
-const targetMailNameTest * = "Florian Rauls" # Debug Option
-
-const smtpServer * = "smtp.googlemail.com" # Address of the smtp server you want to use
-const smtpServerPort * = "465" # Port of the smtp server you want to use
